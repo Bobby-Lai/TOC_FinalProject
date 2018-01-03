efa2b4a3856bc1bcd3110905d4b49c88a1ccbab3
賴柏瑜 <bobby92307@gmail.com>
2018-01-03 23:18:03 +0800

    v.1

---
+++ app.py
@@ -0,0 +1,125 @@
+import sys
+from io import BytesIO
+
+import telegram
+from flask import Flask, request, send_file
+
+from fsm import TocMachine
+
+
+API_TOKEN = '500005505:AAGn3xAUZGyg2l5x-_ORRiArYbs7JCkX1XY'
+WEBHOOK_URL = 'https://61ed9173.ngrok.io/hook'
+
+app = Flask(__name__)
+bot = telegram.Bot(token=API_TOKEN)
+machine = TocMachine(
+    states=[
+        'user',
+        'jokelist',
+        'jokeinorder',
+        'joke1',
+        'joke2',
+        'joke3',
+        'joke4',
+        'joke5'
+    ],
+    transitions=[
+        {
+            'trigger': 'advance',
+            'source': 'user',
+            'dest': 'jokelist',
+            'conditions': 'is_going_to_jokelist'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'user',
+            'dest': 'jokeinorder',
+            'conditions': 'is_going_to_jokeinorder'
+        },
+         {
+            'trigger': 'advance',
+            'source': 'jokelist',
+            'dest': 'user',
+            'conditions': 'jokelist_to_home'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'jokeinorder',
+            'dest': 'user',
+            'conditions': 'jokeinorder_to_home'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'jokelist',
+            'dest': 'joke1',
+            'conditions': 'list_to_joke1'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'jokelist',
+            'dest': 'joke2',
+            'conditions': 'list_to_joke2'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'jokelist',
+            'dest': 'joke3',
+            'conditions': 'list_to_joke3'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'jokelist',
+            'dest': 'joke4',
+            'conditions': 'list_to_joke4'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'jokelist',
+            'dest': 'joke5',
+            'conditions': 'list_to_joke5'
+        },
+        {
+            'trigger': 'go_back',
+            'source': [
+                'joke1',
+                'joke2',
+                'joke3',
+                'joke4',
+                'joke5'
+            ],
+            'dest': 'jokelist'
+        }
+    ],
+    initial='user',
+    auto_transitions=False,
+    show_conditions=True,
+)
+
+
+def _set_webhook():
+    status = bot.set_webhook(WEBHOOK_URL)
+    if not status:
+        print('Webhook setup failed')
+        sys.exit(1)
+    else:
+        print('Your webhook URL has been set to "{}"'.format(WEBHOOK_URL))
+
+
+@app.route('/hook', methods=['POST'])
+def webhook_handler():
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
+    machine.advance(update)
+    return 'ok'
+
+
+@app.route('/show-fsm', methods=['GET'])
+def show_fsm():
+    byte_io = BytesIO()
+    machine.graph.draw(byte_io, prog='dot', format='png')
+    byte_io.seek(0)
+    return send_file(byte_io, attachment_filename='fsm.png', mimetype='image/png')
+
+
+if __name__ == "__main__":
+    _set_webhook()
+    app.run()
---
+++ fsm.py
@@ -0,0 +1,100 @@
+from transitions.extensions import GraphMachine
+
+
+class TocMachine(GraphMachine):
+    def __init__(self, **machine_configs):
+        update.message.reply_text("Hi I'm a chatbot which can tell jokes\nenter \"list\" to see the jokelist\nenter \"joke\" to hear all jokes in order"
+        self.machine = GraphMachine(
+            model = self,
+            **machine_configs
+        )
+
+    def is_going_to_jokelist(self, update):
+        text = update.message.text
+        return text.lower() == 'list'
+
+    def is_going_to_jokeinorder(self, update):
+        text = update.message.text
+        return text.lower() == 'joke'
+
+    def jokelist_to_home(self, update):
+        text = update.message.text
+        return text.lower() == 'home'
+
+    def jokeinorder_to_home(self, update):
+        text = update.message.text
+        return text.lower() == 'home'
+
+    def list_to_joke1(self, update):
+        text = update.message.text
+        return text.lower() == 'joke1'
+
+    def list_to_joke2(self, update):
+        text = update.message.text
+        return text.lower() == 'joke2'
+
+    def list_to_joke3(self, update):
+        text = update.message.text
+        return text.lower() == 'joke3'
+
+    def list_to_joke4(self, update):
+        text = update.message.text
+        return text.lower() == 'joke4'
+
+    def list_to_joke5(self, update):
+        text = update.message.text
+        return text.lower() == 'joke5'
+
+
+
+    def on_enter_jokelist(self, update):
+        update.message.reply_text("Which joke do you want to hear?\n1. Emergency\n2. Say Daddy\n3. Time to Live\n4. Fancy Stuff\n5. No Problem\nPlease enter \"joke(number)\" you want to hear, (for example:joke1)\n\nEnter \"home\" to go to the home page")
+        
+    def on_exit_jokelist(self, update):
+        print('Leaving jokelist')
+
+    def on_enter_jokeinorder(self, update):
+        update.message.reply_text("Boy: *calls 911* Hello? I need your help!\n911: Alright, What is it?\nBoy: Two girls are fighting over me!\n911: So what's your emergency?\nBoy: The ugly one is winning.")
+        update.message.reply_text("Dad: Say daddy!\nBaby: Mommy!\nDad: Come on, say daddy!\nBaby: Mommy!\nDad: F*ck you, say daddy!\nBaby: F*ck you, Mommy!\nMom: Honey, I'm home!\nBaby: F*ck you!\nMom: Who taught you that?\nBaby: Daddy!\nDad: Son of a b*tch.")
+        update.message.reply_text("Doctor: I'm sorry but you suffer from a terminal illness and have only 10 to live.\nPatient: What do you mean, 10? 10 what? Months? Weeks?!\nDoctor: Nine.")
+        update.message.reply_text("Mother: How was school today, Patrick?\nPatrick: It was really great mum! Today we made explosives!\nMother: Ooh, they do very fancy stuff with you these days. And what will you do at school tomorrow?\nPatrick: What school?")
+        update.message.reply_text("Today I went to a barber’s shop for a shave. The barber asked me to put a small wooden ball in my mouth so he could get a closer shave around my cheeks.\nI asked: But what if I swallow the ball?\nHe replied: No problem sir, you just bring it back tomorrow like everybody else.")
+        update.message.reply_text("Enter \"home\" to go to the home page")
+
+    def on_exit_jokeinorder(self, update):
+        print('Leaving jokeinorder')
+
+    def on_enter_joke1(self, update):
+        update.message.reply_text("Boy: *calls 911* Hello? I need your help!\n911: Alright, What is it?\nBoy: Two girls are fighting over me!\n911: So what's your emergency?\nBoy: The ugly one is winning.")
+        self.go_back(update)
+
+    def on_exit_joke1(self, update):
+        print('Leaving joke1')
+
+    def on_enter_joke2(self, update):
+        update.message.reply_text("Dad: Say daddy!\nBaby: Mommy!\nDad: Come on, say daddy!\nBaby: Mommy!\nDad: F*ck you, say daddy!\nBaby: F*ck you, Mommy!\nMom: Honey, I'm home!\nBaby: F*ck you!\nMom: Who taught you that?\nBaby: Daddy!\nDad: Son of a b*tch.")
+        self.go_back(update)
+        
+    def on_exit_joke2(self, update):
+        print('Leaving joke2')
+
+    def on_enter_joke3(self, update):
+        update.message.reply_text("Doctor: I'm sorry but you suffer from a terminal illness and have only 10 to live.\nPatient: What do you mean, 10? 10 what? Months? Weeks?!\nDoctor: Nine.")
+        self.go_back(update)
+        
+    def on_exit_joke3(self, update):
+        print('Leaving joke3')
+
+    def on_enter_joke4(self, update):
+        update.message.reply_text("Mother: How was school today, Patrick?\nPatrick: It was really great mum! Today we made explosives!\nMother: Ooh, they do very fancy stuff with you these days. And what will you do at school tomorrow?\nPatrick: What school?")
+        self.go_back(update)
+        
+    def on_exit_joke4(self, update):
+        print('Leaving joke4')
+
+    def on_enter_joke5(self, update):
+        update.message.reply_text("Today I went to a barber’s shop for a shave. The barber asked me to put a small wooden ball in my mouth so he could get a closer shave around my cheeks.\nI asked: But what if I swallow the ball?\nHe replied: No problem sir, you just bring it back tomorrow like everybody else.")
+        self.go_back(update)
+        
+    def on_exit_joke5(self, update):
+        print('Leaving joke5')
